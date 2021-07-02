2169731843176ea608c563c04ae45c5d62249f37
Jasonzhu0314 <jasonzhu_0314@163.com>
2021-07-02 15:59:13 +0800

    add wrap && modified fork bug

--- forks/tcp_client.cc
+++ forks/tcp_client.cc
@@ -3,52 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include "tcp_client.h"  
-
-#define PRR_EXIT(msg) { \
-	perror(msg); \
-	exit(1); \
-}
+#include "../wrap.h"
 
 tcp_client::tcp_client(char* server_ip, char* server_port)  
 {  
-    if ((socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-        //指定期望的通信协议类型，返回的文件描述符和套接字描述符类似，我们成为套接字描述符，简称sockfd
-        printf("create socket error: %s(errno:%d)\n)",strerror(errno),errno);  
-        exit(0);  
-    }  
-    printf("client socket fd: %d\n", socket_fd);
-
+    socket_fd = Socket(AF_INET, SOCK_STREAM, 0);
+    //指定通信协议，返回的文件描述符和套接字描述符类似，简称sockfd
     bzero(&server_addr, 0);  
     server_addr.sin_family = AF_INET;  
     server_addr.sin_port = htons(atoi(server_port));  
 
     if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <=0 ) {  
-        PRR_EXIT("inet_pton")
+        perr_exit("inet_pton");
     }  
 
-    if (connect(socket_fd, (struct sockaddr* ) &server_addr, sizeof(server_addr) ) < 0) {  
-        // 创建ip和socket套接字的连接
-        PRR_EXIT("connect")
-    }  
+    Connect(socket_fd, (struct sockaddr* ) &server_addr, sizeof(server_addr));
+    // 创建ip和socket套接字的连接
     int n;
-    //while(1) {
+    while (1) {
         printf("send message to server: \n"); 
-        bzero(message, MAX_NUM); 
-        if ( (n = read(STDIN_FILENO, message, MAX_NUM)) < 0) {
-            PRR_EXIT("read");
-        }
-        //if ( !strncmp(message, "quit", 4)) break;        
-        // // 发送数据
-        if (write(socket_fd, message, n) < 0) {
-            PRR_EXIT("send message error\n");
-        } 
-
-        //bzero(message, sizeof(message));
-        //if ( (n = read(socket_fd, message, MAX_NUM) ) < 0)
-        //    PRR_EXIT("read from server")
-        //write(1, message, n);
-    //}
-
+        n = Read(STDIN_FILENO, message, MAX_NUM);
+        if (!strncmp(message, "quit", 4)) break;        
+        // 发送数据
+        Write(socket_fd, message, n);
+        n = Read(socket_fd, message, MAX_NUM);
+        Write(1, message, n);
+    }
     close(socket_fd);  
 }  
 
--- forks/tcp_server.cc
+++ forks/tcp_server.cc
@@ -6,56 +6,40 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include "tcp_server.h"
-
-#define PRR_EXIT(msg) { \
-	perror(msg); \
-	exit(1); \
-}
+#include "../wrap.h"
 
 #define MAX_CONNECT_NUMS 4
 
 tcp_server::tcp_server(int listen_port) {
-    if (( socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 ) {
-		PRR_EXIT("socket error");
-    }
+	socket_fd = Socket(AF_INET, SOCK_STREAM, 0);
 
 	memset(&myserver, 0, sizeof(myserver));
 	myserver.sin_family = AF_INET;
 	myserver.sin_addr.s_addr = htonl(INADDR_ANY);
 	myserver.sin_port = htons(listen_port);
 
-	if (bind(socket_fd, (sockaddr*) &myserver, sizeof(myserver)) < 0 ) {
-		// 本地ip绑定套接口
-		PRR_EXIT("bind failed");
-	}
-
-	if( listen(socket_fd, MAX_CONNECT_NUMS) < 0 ) {
-		//开始监听
-		PRR_EXIT("listen failed");
-	}
+	Bind(socket_fd, (struct sockaddr* ) &myserver, sizeof(myserver));
+	Listen(socket_fd, MAX_CONNECT_NUMS);
 }
  
 int tcp_server::recv_msg() {
 	while (1) {
 		socklen_t sin_size = sizeof(struct sockaddr_in);
-		if(( accept_fd = accept(socket_fd, (struct sockaddr*) &remote_addr, &sin_size)) == -1 ) {
-			PRR_EXIT("accept");
-		}
+		accept_fd = Accept(socket_fd, (struct sockaddr*) &remote_addr, &sin_size);
 		printf("Received a connection from %s:port %d\n", \
 				inet_ntoa(remote_addr.sin_addr), htons(remote_addr.sin_port));
 
 		pid_t pid = fork();
-		if (pid < 0) PRR_EXIT("fork");
+		if (pid < 0)  perr_exit("fork");
 		if (pid > 0) {
-			// close(accept_fd); // 父进程不需要客户端文件描述符
+			close(accept_fd); // 父进程不需要客户端文件描述符
 			while(waitpid(-1, NULL, WNOHANG) < 0) {}; // 非阻塞收尸
 			continue;
 		}
-
 		// 创建子进程处理客户端连接，否则，多个客户端连接时，同一时间，只能响应一个客户端
-		// close(socket_fd);
-		
+		close(socket_fd);
 		tcp_server::process_client(accept_fd);
+		break;
 	}
 	return 0;
 }
@@ -64,19 +48,18 @@ void tcp_server::process_client(int accept_fd) {
 	int n, i;
 	char buf[MAXSIZE] = {0};
 	while(1) {
-		if ( (n = read(accept_fd, buf, MAXSIZE)) < 0 ) {
-			PRR_EXIT("read:");
+		n = Read(accept_fd, buf, MAXSIZE);
+		if (n == 0 || !strncmp(buf, "quit", 4)) {
+			close(accept_fd);
+			break;
 		}
-		
-		write(STDOUT_FILENO, buf, n);
-		if (!strncmp(buf, "quit", 4)) break;
-		//for(i = 0; i < n; i++) {
-		//	buf[i] = toupper(buf[i]);
-		//}
-		//write(accept_fd, buf, n);
+		Write(STDOUT_FILENO, buf, n);
+		for(i = 0; i < n; i++) {
+			buf[i] = toupper(buf[i]);
+		}
+		Write(accept_fd, buf, n);
 	}
-	
-	// 不超过长度的时候，有多少读多少，当超过最大的MAXSIZE的时候处理机制
+	// TODO:不超过长度的时候，有多少读多少，当超过最大的MAXSIZE的时候处理机制
 	printf("server  end connect\n");
 	close(accept_fd);
 }
