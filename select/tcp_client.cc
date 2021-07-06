4adf1191df3ca4372577869c7c8fb0b16a55eaae
Jasonzhu0314 <jasonzhu_0314@163.com>
2021-07-06 22:47:41 +0800

    merge select

--- select/tcp_client.cc
+++ select/tcp_client.cc
@@ -3,52 +3,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include "tcp_client.h"  
-
-#define PRR_EXIT(msg) { \
-	perror(msg); \
-	exit(1); \
-}
+#include "wrap.h"
 
 tcp_client::tcp_client(char* server_ip, char* server_port)  
 {  
-    if ((socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-        // 指定期望的通信协议类型，返回的文件描述符和套接字描述符类似，我们成为套接字描述符，简称sockfd
-        PRR_EXIT("create socket")  
-    }  
-
+    socket_fd = Socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    // 指定期望的通信协议类型，返回的文件描述符和套接字描述符类似，我们成为套接字描述符，简称sockfd
     memset(&server_addr, 0, sizeof(server_addr));  
     server_addr.sin_family = AF_INET;  
     server_addr.sin_port = htons(atoi(server_port));  
 
     if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <=0 ) {  
-        PRR_EXIT("inet_pton")
+        perr_exit("inet_pton");
     }  
 
-    if (connect(socket_fd, (struct sockaddr* ) &server_addr, sizeof(server_addr) ) < 0) {  
-        // 创建ip和socket套接字的连接
-        PRR_EXIT("connect") 
-    }  
-
+    Connect(socket_fd, (struct sockaddr* ) &server_addr, sizeof(server_addr) );
+    // 创建ip和socket套接字的连接
     int n;
-
     while (1) {
         printf("send message to server: \n");
-        if ((n = read(0, message, MAXSIZE)) < 0) {
-            PRR_EXIT("read");
-        }  
+        n = Read(0, message, MAXSIZE);
         // 发送数据
+        Write(socket_fd, message, n);
         if (!strncmp(message, "quit", 4)) {
            break; 
         }
-        if ((n = write(socket_fd, message, n)) < 0) {
-            PRR_EXIT("write");
-        } 
-        if ((n = read(socket_fd, message, MAXSIZE)) < 0) {
-            PRR_EXIT("read from server");
-        }  
-        if ((n = write(STDOUT_FILENO, message, n)) < 0) {
-            PRR_EXIT("write to stdout");
-        } 
+        n = Read(socket_fd, message, MAXSIZE);
+        Write(STDOUT_FILENO, message, n);
     }
     close(socket_fd);  
     // TODO: shutdown函数能只关闭读缓存或写缓存中的一个
--- select/tcp_server.cc
+++ select/tcp_server.cc
@@ -1,13 +1,11 @@
 #include <string.h>
-#include <cstdio>
+#include <stdio.h>
 #include <stdlib.h>
-#include <thread>
 #include "tcp_server.h"
+#include "wrap.h"
 
 tcp_server::tcp_server(int listen_port) {
-    if (( socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 ) {
-		throw "socket() failed";
-    }
+    socket_fd = Socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	memset(&myserver, 0, sizeof(myserver));
 	myserver.sin_family = AF_INET;
@@ -15,14 +13,10 @@ tcp_server::tcp_server(int listen_port) {
 	inet_aton("127.0.0.1", &myserver.sin_addr);
 	myserver.sin_port = htons(listen_port);
 
-	if (bind(socket_fd, (sockaddr*) &myserver, sizeof(myserver)) < 0 ) {
-		// 本地ip绑定套接口
-		throw "bind() failed";
-	}
-	if( listen(socket_fd, 128) < 0 ) {
-		//开始监听
-		throw "listen() failed";
-	}
+	// 本地ip绑定套接口
+	Bind(socket_fd, (sockaddr*) &myserver, sizeof(myserver));
+	//开始监听
+	Listen(socket_fd, 128); 
 	
 	socklen_t sin_size = sizeof(struct sockaddr_in);
 	int maxfd = socket_fd;
@@ -44,20 +38,18 @@ tcp_server::tcp_server(int listen_port) {
 		read_set = all_set;
 		nready = select(maxfd + 1, &read_set, NULL, NULL, NULL);
 		// 初始为0,1,2,3 共maxfd + 1个，新创建的文件描述符一定是编号中最小的，会阻塞等待网卡产生信号
-		if (nready < 0)
-            break;
+		if (nready < 0)  {
+			break;
+		}
 		if (FD_ISSET(socket_fd, &read_set)) { // 判断监听socket口是否有变化
-			
-			if (( accept_fd = accept(socket_fd, (struct sockaddr*) &remote_addr, &sin_size)) == -1) {
-				throw "Accept error!";
-				continue;
-			}
+			accept_fd = Accept(socket_fd, (struct sockaddr*) &remote_addr, &sin_size);
 			printf("Received a connection from %s:%d\n",
 					(char*) inet_ntoa(remote_addr.sin_addr),
 					ntohs(remote_addr.sin_port));
 			
-			for (int i = 0; i < FD_SETSIZE; i++) { //可优化
+			for (i = 0; i < FD_SETSIZE; i++) { //可优化
 				if (client_fd_set[i] < 0) {  // 将创建的socket放到clien_fd_set队列中
+					printf("new connect[%d]:%d\n", i, accept_fd);
 					client_fd_set[i] = accept_fd;
 					current_fd_num = i;
 					break;
@@ -66,7 +58,6 @@ tcp_server::tcp_server(int listen_port) {
 
 			if (current_fd_num == FD_SETSIZE) {
 				fputs("too many clients\n",stderr);
-                exit(1);
 			}
 
 			FD_SET(accept_fd, &all_set);
@@ -76,32 +67,25 @@ tcp_server::tcp_server(int listen_port) {
 			if (--nready == 0) continue;
 		}
 		else {
-			printf("select get read signal\n");
 			for (i = 0; i < FD_SETSIZE; i++) {
-				// 可以使用
 				accept_fd = client_fd_set[i];
 				if (accept_fd < 0) continue;
+
 				if (FD_ISSET(accept_fd, &read_set)) {
-					n = read(accept_fd, buf, MAXSIZE);
-					if (n == 0) {
-						// 没有数据，证明socket口关闭，要将标记位清除，否者select返回为1
-						// write(accept_fd, "byebye", 6);
-						// sleep(2);
+					n = Read(accept_fd, buf, MAXSIZE);
+					if (n == 0 || !strncmp(buf, "quit", 4)) {
 						close(accept_fd);
 						FD_CLR(accept_fd, &all_set);
-						printf("closed client: %d \n", i);
+						client_fd_set[i] = -1;
+						printf("closed client[%d]:%d \n", i, accept_fd);
+					    if (--nready==0) break;
 						continue;
 					} else if (n > 0) {
-						if (!strncmp(buf, "quit", 4)) {
-							close(accept_fd);
-							FD_CLR(accept_fd, &all_set);
-							printf("closed client: %d \n", i);
-						}
-						write(1, buf, n);
+						Write(1, buf, n);
 						for(i = 0; i < n; i++) {
 							buf[i] = toupper(buf[i]);
 						}
-						write(accept_fd, buf, n);
+						Write(accept_fd, buf, n);
 					}
 					if (--nready==0) break;
 				}
